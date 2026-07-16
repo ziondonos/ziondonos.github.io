19064237a1d8feff54d141047314449253ea761c
ziondonos <158517670+ziondonos@users.noreply.github.com>
2026-07-16 17:44:30 +0100

    Update direct function to include mail link

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 function direct(){
-  document.getElementById("fb").innerHTML='<a href="https://www.facebook.com/zion.david.728617" target="_blank">FACEBOOK</a>';
+  document.getElementById("fb").innerHTML='<a href="mailto:ziondavidonosiovwe1" class="btn mail-btn">MAIL</a><br>'+'<a href="https://www.facebook.com/zion.david.728617" target="_blank" class="btn fb-btn">FACEBOOK</a>';
     }
