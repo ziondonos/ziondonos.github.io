be39ad2f75f583851db7265869b93c53da68ab59
Zion <158517670+ziondonos@users.noreply.github.com>
2026-07-20 08:32:23 +0100

    Delete script.js

--- script.js
+++
@@ -1,3 +0,0 @@
-function direct(){
-  document.getElementById("fb").innerHTML='<a href="mailto:ziondavidonosiovwe1" class="btn mail-btn">MAIL</a><br>'+'<a href="https://www.facebook.com/zion.david.728617" target="_blank" class="btn fb-btn">FACEBOOK</a>';
-    }
